45244bcf35139ffc32494d0c5c84efcd65805225
dkcsn <christian_soegaard@icloud.com>
2026-06-01 20:06:00 +0200

    Add explicit clear selections action

--- Sonos2MatrixMapping.lua
+++ Sonos2MatrixMapping.lua
@@ -2,7 +2,7 @@
 -- Finds Sonos Manager children, Yahue devices and Logic Group Matrix devices.
 
 local APP_NAME = "Matrix Button Configuration"
-local APP_VERSION = "1.2.44"
+local APP_VERSION = "1.2.45"
 local DEFAULT_SOURCE_LIST = { 1, 2, 3, 11, 12, 13 }
 local DEFAULT_BACKUP_GLOBAL_NAME = "MatrixButtonConfigurationBackup"
 local DEFAULT_BUTTON_PROFILES = {
@@ -1310,8 +1310,7 @@ function QuickApp:saveMapping()
   }
 
   self:setVariable("mapping", encodeJson(self.mapping))
-  self:updateView("info", "text", "Mapping gemt -> Matrix " .. table.concat(matrixIds, ", ") .. " - destinationer og knapper nulstillet")
-  self:clearCurrentSelections()
+  self:updateView("info", "text", "Mapping gemt -> Matrix " .. table.concat(matrixIds, ", "))
   self:updateSummary()
 end
 
@@ -1320,12 +1319,17 @@ function QuickApp:clearCurrentSelections()
   self.selectedYahueId = nil
   self.selectedTahomaId = nil
   self.selectedTahomaIds = {}
+  self.pendingMatrixIds = {}
   self.buttonConfig = normalizeButtonConfig(DEFAULT_BUTTON_CONFIG)
+  self:setVariable("selectedMatrixIds", "[]")
 
   updateSelectedItems(self, "sonosSelect", {})
   updateSelectedItems(self, "yahueSelect", {})
   updateSelectedItems(self, "tahomaSelect", {})
+  updateSelectedItems(self, "matrixSelect", {})
   self:updateButtonProfileSelections()
+  self:updateView("info", "text", "Valg nulstillet")
+  self:updateSummary()
 end
 
 function QuickApp:clearMapping()
