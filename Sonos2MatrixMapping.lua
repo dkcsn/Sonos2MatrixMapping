719b61e46a7c59e6fac245d5fc4b150711a479b4
dkcsn <christian_soegaard@icloud.com>
2026-06-01 19:57:23 +0200

    Keep selected Matrix after save

--- Sonos2MatrixMapping.lua
+++ Sonos2MatrixMapping.lua
@@ -2,7 +2,7 @@
 -- Finds Sonos Manager children, Yahue devices and Logic Group Matrix devices.
 
 local APP_NAME = "Matrix Button Configuration"
-local APP_VERSION = "1.2.43"
+local APP_VERSION = "1.2.44"
 local DEFAULT_SOURCE_LIST = { 1, 2, 3, 11, 12, 13 }
 local DEFAULT_BACKUP_GLOBAL_NAME = "MatrixButtonConfigurationBackup"
 local DEFAULT_BUTTON_PROFILES = {
@@ -1310,7 +1310,7 @@ function QuickApp:saveMapping()
   }
 
   self:setVariable("mapping", encodeJson(self.mapping))
-  self:updateView("info", "text", "Mapping gemt -> Matrix " .. table.concat(matrixIds, ", "))
+  self:updateView("info", "text", "Mapping gemt -> Matrix " .. table.concat(matrixIds, ", ") .. " - destinationer og knapper nulstillet")
   self:clearCurrentSelections()
   self:updateSummary()
 end
@@ -1320,14 +1320,11 @@ function QuickApp:clearCurrentSelections()
   self.selectedYahueId = nil
   self.selectedTahomaId = nil
   self.selectedTahomaIds = {}
-  self.pendingMatrixIds = {}
   self.buttonConfig = normalizeButtonConfig(DEFAULT_BUTTON_CONFIG)
-  self:setVariable("selectedMatrixIds", "[]")
 
   updateSelectedItems(self, "sonosSelect", {})
   updateSelectedItems(self, "yahueSelect", {})
   updateSelectedItems(self, "tahomaSelect", {})
-  updateSelectedItems(self, "matrixSelect", {})
   self:updateButtonProfileSelections()
 end
 
